98915ed580f777845b4098261c7b0852e196659a
신동구 <ggg9905@naver.com>
2023-10-21 23:39:02 +0900

    개인프로젝트 준비 세팅

---
+++ src/components/common/FooterCom.tsx
@@ -0,0 +1,14 @@
+﻿import React from 'react'
+
+function FooterCom() {
+  return (
+    <div className="">
+       <p className="fs-3 wow bounceIn content-img">
+        이미지
+      {/* <img className="VegetablesImage" alt="vegetables" src="../assets/img/vegetables-155616_1280.png" /> */}
+       </p>
+      </div>
+  )
+}
+
+export default FooterCom
---
+++ src/pages/Home.tsx
@@ -0,0 +1,31 @@
+﻿import React, { useEffect } from "react";
+import TitleCom from "../components/common/TitleCom";
+import initWow from "../assets/js/mywow";
+import "../assets/css/home.css"
+
+
+function Home() {
+  useEffect(() => {
+    initWow();
+  });
+  return (
+    <>
+      {/* 제목 start */}
+      <TitleCom title="Body profile" />
+      {/* 제목 end */}
+
+      <div id="content-wrapper">
+        <p className="fs-3 wow bounceIn">
+          건강한 내 몸, 기록하고 싶은 내 몸을 남기는 공간 <br />
+           Body Profile에 오신것을 환영합니다. <br />
+        </p>
+        {/* <!-- 로고 아이콘 저작권 표시 --> */}
+        <a href="https://www.flation.com/kr/free-icons/" title="건반 아이콘">
+          건반 아이콘 제작자: Chanut-is-Industries - Flaticon
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Home;
